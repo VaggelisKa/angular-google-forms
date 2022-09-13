fb8c898eaebe5a0841486a5cadca7f894b1214b5
VaggelisKa <e.karavasileiadis@gmail.com>
2022-09-13 07:39:39 +0200

    Add login functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'feedback-analyzer';
+
+  isAuthenticated$: Observable<boolean>;
+
+  constructor(private _authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated$ = this._authService.isAuthenticated$;
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
+  constructor(private _authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleLogin() {
+    this._authService.loginWithRedirect();
   }
-
 }
